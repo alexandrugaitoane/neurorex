90dc914c6e1d83d3b51cc986d16130fa08eee6ce
alexandru gaitoane <alexandrugaitoane@gmail.com>
2025-04-23 21:57:43 +0300

    First MRI Classifier

---
+++ models/mri_tumor_classifier/cnn_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def mri_tumor_classifier(input_shape=(224, 224, 3), num_classes=4):
+    base_model = tf.keras.applications.MobileNetV2(input_shape=input_shape,
+                                                 include_top=False,
+                                                 weights='imagenet')
+    base_model.trainable = True
+    
+    for layer in base_model.layers[:-30]:
+        layer.trainable = False    
+    inputs = tf.keras.Input(shape=input_shape)
+    x = base_model(inputs, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+    model.summary()
+    return model
