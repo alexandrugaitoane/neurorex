f98da28bf65100a71caaefdb86750fc51b0fa861
alexandru gaitoane <alexandrugaitoane@gmail.com>
2025-04-25 13:27:04 +0300

    Fine-tuned MRI tumor classifier with deep head and updated results

--- models/mri_tumor_classifier/cnn_model.py
+++ models/mri_tumor_classifier/cnn_model.py
@@ -13,6 +13,8 @@ def mri_tumor_classifier(input_shape=(224, 224, 3), num_classes=4):
     x = base_model(inputs, training=False)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
