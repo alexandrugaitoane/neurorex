11d1d73dd25fae18530d3411e1f2b3fa916fdbad
alexandru gaitoane <alexandrugaitoane@gmail.com>
2025-04-26 23:57:21 +0300

    MRI segmentation model fully loaded

--- models/mri_tumor_segmentation/u_net.py
+++ models/mri_tumor_segmentation/u_net.py
@@ -1,52 +1,52 @@
 import tensorflow as tf
 from tensorflow import keras
 
-def unet_model(input_shape=(224, 224, 3)):
-    inputs = tf.keras.Input(input_shape)
 
-    # Contracting path
-    c1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
-    c1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(c1)
-    p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+def build_model():
+    inputs = tf.keras.Input(shape=(224, 224, 3), name="input_layer")
 
-    c2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(p1)
-    c2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(c2)
-    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+    # Encoder
+    c1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", name="conv2d")(inputs)
+    c1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", name="conv2d_1")(c1)
+    p1 = tf.keras.layers.MaxPooling2D((2, 2), name="max_pooling2d")(c1)
 
-    c3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(p2)
-    c3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(c3)
-    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+    c2 = tf.keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same", name="conv2d_2")(p1)
+    c2 = tf.keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same", name="conv2d_3")(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2), name="max_pooling2d_1")(c2)
 
-    c4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(p3)
-    c4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(c4)
-    p4 = tf.keras.layers.MaxPooling2D((2, 2))(c4)
+    c3 = tf.keras.layers.Conv2D(256, (3, 3), activation="relu", padding="same", name="conv2d_4")(p2)
+    c3 = tf.keras.layers.Conv2D(256, (3, 3), activation="relu", padding="same", name="conv2d_5")(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2), name="max_pooling2d_2")(c3)
 
-    # Bottleneck
-    c5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same')(p4)
-    c5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same')(c5)
+    c4 = tf.keras.layers.Conv2D(512, (3, 3), activation="relu", padding="same", name="conv2d_6")(p3)
+    c4 = tf.keras.layers.Conv2D(512, (3, 3), activation="relu", padding="same", name="conv2d_7")(c4)
+    p4 = tf.keras.layers.MaxPooling2D((2, 2), name="max_pooling2d_3")(c4)
 
-    # Expanding path
-    u6 = tf.keras.layers.Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(c5)
-    u6 = tf.keras.layers.concatenate([u6, c4])
-    c6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(u6)
-    c6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same')(c6)
+    c5 = tf.keras.layers.Conv2D(1024, (3, 3), activation="relu", padding="same", name="conv2d_8")(p4)
+    c5 = tf.keras.layers.Conv2D(1024, (3, 3), activation="relu", padding="same", name="conv2d_9")(c5)
 
-    u7 = tf.keras.layers.Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(c6)
-    u7 = tf.keras.layers.concatenate([u7, c3])
-    c7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(u7)
-    c7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same')(c7)
+    # Decoder
+    u6 = tf.keras.layers.Conv2DTranspose(512, (2, 2), strides=(2, 2), padding="same", name="conv2d_transpose")(c5)
+    u6 = tf.keras.layers.Concatenate(name="concatenate")([u6, c4])
+    c6 = tf.keras.layers.Conv2D(512, (3, 3), activation="relu", padding="same", name="conv2d_10")(u6)
+    c6 = tf.keras.layers.Conv2D(512, (3, 3), activation="relu", padding="same", name="conv2d_11")(c6)
 
-    u8 = tf.keras.layers.Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(c7)
-    u8 = tf.keras.layers.concatenate([u8, c2])
-    c8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(u8)
-    c8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same')(c8)
+    u7 = tf.keras.layers.Conv2DTranspose(256, (2, 2), strides=(2, 2), padding="same", name="conv2d_transpose_1")(c6)
+    u7 = tf.keras.layers.Concatenate(name="concatenate_1")([u7, c3])
+    c7 = tf.keras.layers.Conv2D(256, (3, 3), activation="relu", padding="same", name="conv2d_12")(u7)
+    c7 = tf.keras.layers.Conv2D(256, (3, 3), activation="relu", padding="same", name="conv2d_13")(c7)
 
-    u9 = tf.keras.layers.Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(c8)
-    u9 = tf.keras.layers.concatenate([u9, c1])
-    c9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(u9)
-    c9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same')(c9)
+    u8 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same", name="conv2d_transpose_2")(c7)
+    u8 = tf.keras.layers.Concatenate(name="concatenate_2")([u8, c2])
+    c8 = tf.keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same", name="conv2d_14")(u8)
+    c8 = tf.keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same", name="conv2d_15")(c8)
 
-    outputs = tf.keras.layers.Conv2D(1, 1, activation='sigmoid')(c9)
+    u9 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same", name="conv2d_transpose_3")(c8)
+    u9 = tf.keras.layers.Concatenate(name="concatenate_3")([u9, c1])
+    c9 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", name="conv2d_16")(u9)
+    c9 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", name="conv2d_17")(c9)
 
-    model = tf.keras.models.Model(inputs, outputs)
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation="sigmoid", name="conv2d_18")(c9)
+
+    model = tf.keras.models.Model(inputs, outputs, name="functional")
     return model
